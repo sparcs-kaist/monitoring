4bcb5c0a5d3068e6b4cc0a375809929c1cebb7b5
DoyunShin <doyun.shin@gmail.com>
2025-06-24 11:01:16 +0900

    fix: Kong server use host mysqld exporter

---
+++ targets/kong.sparcs.net/run.sh
@@ -0,0 +1,22 @@
+sudo true
+sudo tee /etc/systemd/system/mysqld-exporter.service <<'EOF'
+[Unit]
+Description=Prometheus MySQL Exporter
+After=network.target
+User=wheel
+Group=wheel
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=/opt/mysqld_exporter/mysqld_exporter \
+    --config.my-cnf=/etc/mysqld_exporter.my.cnf \
+    --mysqld.address=127.0.0.1:3006
+
+EOF
+
+sudo systemctl daemon-reload
+
+printf "Run \`sudo systemctl enable --now mysqld-exporter.service\` to run exporter\n"
+printf "Make sure \`/etc/mysqld_exporter.my.cnf\` exists\n"
+
