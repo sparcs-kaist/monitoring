498df54809387571c0b71637b1ed7021cdf665fe
larrykwon <koallarry11@kaist.ac.kr>
2025-07-22 11:18:46 +0900

    Add: add collect flag on mysql exporter

--- targets/channeltalk.sparcs.net/run.sh
+++ targets/channeltalk.sparcs.net/run.sh
@@ -12,6 +12,47 @@ Restart=always
 ExecStart=/opt/mysqld_exporter/mysqld_exporter \
     --config.my-cnf=/etc/mysqld_exporter.my.cnf \
     --mysqld.address=127.0.0.1:3006
+    --collect.engine_innodb_status \
+    --collect.global_status \
+    --collect.global_variables \
+    --collect.info_schema.clientstats \
+    --collect.info_schema.innodb_metrics \
+    --collect.info_schema.innodb_tablespaces \
+    --collect.info_schema.innodb_cmp \
+    --collect.info_schema.innodb_cmpmem \
+    --collect.info_schema.processlist \
+    --collect.info_schema.processlist.min_time=0 \
+    --collect.info_schema.query_response_time \
+    --collect.info_schema.replica_host \
+    --collect.info_schema.tables \
+    --collect.info_schema.tables.databases=‘*’ \
+    --collect.info_schema.tablestats \
+    --collect.info_schema.schemastats \
+    --collect.info_schema.userstats \
+    --collect.mysql.user \
+    --collect.perf_schema.eventsstatements \
+    --collect.perf_schema.eventsstatements.digest_text_limit=120 \
+    --collect.perf_schema.eventsstatements.limit=250 \
+    --collect.perf_schema.eventsstatements.timelimit=86400 \
+    --collect.perf_schema.eventsstatementssum \
+    --collect.perf_schema.eventswaits \
+    --collect.perf_schema.file_events \
+    --collect.perf_schema.file_instances \
+    --collect.perf_schema.file_instances.remove_prefix=false \
+    --collect.perf_schema.indexiowaits \
+    --collect.perf_schema.memory_events \
+    --collect.perf_schema.memory_events.remove_prefix=false \
+    --collect.perf_schema.tableiowaits \
+    --collect.perf_schema.tablelocks \
+    --collect.perf_schema.replication_group_members \
+    --collect.perf_schema.replication_group_member_stats \
+    --collect.perf_schema.replication_applier_status_by_worker \
+    --collect.slave_status \
+    --collect.slave_hosts \
+    --collect.heartbeat \
+    --collect.heartbeat.database=true \
+    --collect.heartbeat.table=true \
+    --collect.heartbeat.utc
 
 [Install]
 WantedBy=multi-user.target
